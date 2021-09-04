8c0d14108fb6bbb55f08ab332b1418b412247676
Ayan-Paul <paul.ayan@outlook.com>
2021-09-04 22:18:02 +0530

    first commit

---
+++ carzone/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
+    path('cars/', include('cars.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('socialaccounts/', include('allauth.urls')),
+    path('contacts/', include('contacts.urls')),
+] + static(settings.MEDIA_URL,serve, document_root=settings.MEDIA_ROOT)
